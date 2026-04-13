ed88d57cd374ff951b99462dc779956c2ccfd99b
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-04-13 11:10:35 +0200

    Add Django project configuration

---
+++ backend/quizly/wsgi.py
@@ -0,0 +1,7 @@
+"""WSGI configuration for Quizly."""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizly.settings")
+application = get_wsgi_application()
