47fef33fe03e4246dfac324bd2a18fa0e2606d63
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-04-13 12:23:07 +0200

    Remove duplicate root backend files

--- quizly/wsgi.py
+++
@@ -1,7 +0,0 @@
-"""WSGI-Konfiguration für Quizly."""
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizly.settings")
-application = get_wsgi_application()
