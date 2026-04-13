931984a36dc8f9f7863b028507322832850ecdc7
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-04-13 11:10:39 +0200

    Add quiz API models and migrations

---
+++ backend/api/authentication.py
@@ -0,0 +1,14 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.conf import settings
+
+
+class CookieJWTAuthentication(JWTAuthentication):
+    """Reads the access token from an HTTP-only cookie instead of the header."""
+
+    def authenticate(self, request):
+        """Extracts the token from the cookie and authenticates the user."""
+        access_token = request.COOKIES.get(settings.SIMPLE_JWT["AUTH_COOKIE"])
+        if not access_token:
+            return None
+        validated_token = self.get_validated_token(access_token)
+        return self.get_user(validated_token), validated_token
